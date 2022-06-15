76ad420b194164d75aa0355a8f70f20c256a62d1
EJBEAN <64283902+itsAnanth@users.noreply.github.com>
2022-06-15 14:36:18 +0530

    fix: reduce range

--- Algorithms/binary_search.cpp
+++ Algorithms/binary_search.cpp
@@ -3,10 +3,10 @@
 int binSearch(int, std::vector<int>);
 
 int main() {
-    std::vector<int>nums = { 10, 5, 4, 3, 2, 1 };
+    std::vector<int>nums = { 10, 5, 4, 6, 3, 2, 1 };
     std::sort(nums.begin(), nums.end());
     
-    int idx = binSearch(5, nums);
+    int idx = binSearch(6, nums);
     
     std::cout << idx;
     return 0;
@@ -15,7 +15,7 @@ int main() {
 int binSearch(int num, std::vector<int> arr) {
     int L = 0, H = arr.size() - 1, idx = -1;
     
-    while (L <= H) {
+    while (L < H) {
         int mid = (L + H) / 2, curr = arr[mid];
         
         if (curr == num) {
